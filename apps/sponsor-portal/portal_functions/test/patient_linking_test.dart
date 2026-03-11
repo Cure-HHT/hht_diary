47be0989ce518f243356da904849bc09f63f4dda
muhammadU-anspar <muhammad@anspar.org>
2026-03-11 13:46:55 -0500

    Remove EQ questionnaire handling from trial start process

--- apps/sponsor-portal/portal_functions/test/patient_linking_test.dart
+++ apps/sponsor-portal/portal_functions/test/patient_linking_test.dart
@@ -1828,11 +1828,6 @@ void main() {
           if (query.contains('UPDATE patients')) {
             return [];
           }
-          if (query.contains('INSERT INTO questionnaire_instances')) {
-            return [
-              ['eq-instance-001'],
-            ];
-          }
           if (query.contains('FROM patient_fcm_tokens')) {
             return [];
           }
@@ -1853,7 +1848,6 @@ void main() {
         final body = await _json(response);
         expect(body['success'], true);
         expect(body['trial_started'], true);
-        expect(body['eq_instance_id'], 'eq-instance-001');
       });
 
       test('returns 403 for non-Investigator role', () async {
@@ -1949,11 +1943,6 @@ void main() {
           }
           if (query.contains('UPDATE patients')) {
             return [];
-          }
-          if (query.contains('INSERT INTO questionnaire_instances')) {
-            return [
-              ['eq-instance-001'],
-            ];
           }
           if (query.contains('FROM patient_fcm_tokens')) {
             return [
