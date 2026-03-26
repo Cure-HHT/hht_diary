3cfaefd6b5c3f68f3bb0fbaf5f275e1475ddf560
muhammadU-anspar <muhammad@anspar.org>
2026-03-26 12:48:34 -0500

    Remove unused sponsor logo and enrollment status from HomeScreen widget

--- apps/daily-diary/clinical_diary/integration_test/home_screen_integration_test.dart
+++ apps/daily-diary/clinical_diary/integration_test/home_screen_integration_test.dart
@@ -49,7 +49,6 @@ import 'package:clinical_diary/utils/timezone_converter.dart';
 import 'package:clinical_diary/widgets/event_list_item.dart';
 import 'package:clinical_diary/widgets/flash_highlight.dart';
 import 'package:clinical_diary/widgets/logo_menu.dart';
-import 'package:clinical_diary/widgets/logo_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
